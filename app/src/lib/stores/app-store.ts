maksym1982/desktop

tracing the interactions to identify what is in-progess

app/src/lib/stores/app-store.ts
@@ -1876,8 +1876,8 @@ export class AppStore extends TypedBaseStore<IAppState> {
   private async recoverMissingRepository(
     repository: Repository
   ): Promise<Repository> {
-    /* 
-        if the repository is marked missing, check to see if the file path exists, 
+    /*
+        if the repository is marked missing, check to see if the file path exists,
         and if so then see if git recognizes the path as a valid repository,
         and if so, reset the missing status as its been restored
       */
@@ -2413,7 +2413,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
       return
     }
 
-    return this.withPushPull(repository, async () => {
+    return this.withPushPull(repository, 'push', async () => {
       const { tip } = state.branchesState
 
       if (tip.kind === TipState.Unborn) {
@@ -2573,13 +2573,21 @@ export class AppStore extends TypedBaseStore<IAppState> {
 
   private async withPushPull(
     repository: Repository,
+    action: string,
     fn: () => Promise<void>
   ): Promise<void> {
     const state = this.repositoryStateCache.get(repository)
     // Don't allow concurrent network operations.
     if (state.isPushPullFetchInProgress) {
+      log.warn(
+        `[withPushPull] action ${action} is being skipped as isPushPullFetchInProgress is already set to true from a previous action`
+      )
       return
     }
+
+    log.warn(
+      `[withPushPull] action ${action} is setting isPushPullFetchInProgress to true`
+    )
 
     this.repositoryStateCache.update(repository, () => ({
       isPushPullFetchInProgress: true,
@@ -2589,6 +2597,10 @@ export class AppStore extends TypedBaseStore<IAppState> {
     try {
       await fn()
     } finally {
+      log.warn(
+        `[withPushPull] action ${action} is setting isPushPullFetchInProgress to false`
+      )
+
       this.repositoryStateCache.update(repository, () => ({
         isPushPullFetchInProgress: false,
       }))
@@ -2607,7 +2619,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
     repository: Repository,
     account: IGitAccount | null
   ): Promise<void> {
-    return this.withPushPull(repository, async () => {
+    return this.withPushPull(repository, 'pull', async () => {
       const gitStore = this.gitStoreCache.get(repository)
       const remote = gitStore.currentRemote
 
@@ -2956,7 +2968,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
     fetchType: FetchType,
     remotes?: IRemote[]
   ): Promise<void> {
-    await this.withPushPull(repository, async () => {
+    await this.withPushPull(repository, 'fetch', async () => {
       const gitStore = this.gitStoreCache.get(repository)
 
       try {

BenjaminJornet/open-desktop

app/src/lib/stores/app-store.ts
@@ -2489,7 +2489,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
       return
     }
 
-    return this.withPushPullFetch(repository, async () => {
+    return this.withPushPullFetch(repository, 'push', async () => {
       const { tip } = state.branchesState
 
       if (tip.kind === TipState.Unborn) {
@@ -2649,13 +2649,21 @@ export class AppStore extends TypedBaseStore<IAppState> {
 
   private async withPushPullFetch(
     repository: Repository,
+    action: string,
     fn: () => Promise<void>
   ): Promise<void> {
     const state = this.repositoryStateCache.get(repository)
     // Don't allow concurrent network operations.
     if (state.isPushPullFetchInProgress) {
+      log.warn(
+        `[withPushPull] action ${action} is being skipped as isPushPullFetchInProgress is already set to true from a previous action`
+      )
       return
     }
+
+    log.warn(
+      `[withPushPull] action ${action} is setting isPushPullFetchInProgress to true`
+    )
 
     this.repositoryStateCache.update(repository, () => ({
       isPushPullFetchInProgress: true,
@@ -2665,6 +2673,10 @@ export class AppStore extends TypedBaseStore<IAppState> {
     try {
       await fn()
     } finally {
+      log.warn(
+        `[withPushPull] action ${action} is setting isPushPullFetchInProgress to false`
+      )
+
       this.repositoryStateCache.update(repository, () => ({
         isPushPullFetchInProgress: false,
       }))
@@ -2683,7 +2695,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
     repository: Repository,
     account: IGitAccount | null
   ): Promise<void> {
-    return this.withPushPullFetch(repository, async () => {
+    return this.withPushPullFetch(repository, 'pull', async () => {
       const gitStore = this.gitStoreCache.get(repository)
       const remote = gitStore.currentRemote
 
@@ -3039,7 +3051,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
     fetchType: FetchType,
     remotes?: IRemote[]
   ): Promise<void> {
-    await this.withPushPullFetch(repository, async () => {
+    await this.withPushPullFetch(repository, 'fetch', async () => {
       const gitStore = this.gitStoreCache.get(repository)
 
       try {