piimask/desktop

add test case for file that is somehow omitted from continuing the rebase

app/test/unit/git/rebase/detect-conflict-test.ts
@@ -278,4 +278,45 @@ describe('git/rebase', () => {
       expect(status.currentTip).not.toBe(beforeRebaseTip.sha)
     })
   })
+
+  describe('continue with tracked change omitted from list', () => {
+    let result: ContinueRebaseResult
+
+    beforeEach(async () => {
+      const repository = await createRepository(baseBranch, featureBranch)
+
+      await rebase(repository, baseBranch, featureBranch)
+
+      // resolve conflicts by writing files to disk
+      await FSE.writeFile(
+        Path.join(repository.path, 'THING.md'),
+        '# HELLO WORLD! \nTHINGS GO HERE\nFEATURE BRANCH UNDERWAY\n'
+      )
+
+      await FSE.writeFile(
+        Path.join(repository.path, 'OTHER.md'),
+        '# HELLO WORLD! \nTHINGS GO HERE\nALSO FEATURE BRANCH UNDERWAY\n'
+      )
+
+      // change unrelated tracked while rebasing changes
+      await FSE.writeFile(
+        Path.join(repository.path, 'THIRD.md'),
+        'this change should be included in the latest commit'
+      )
+
+      const afterRebase = await getStatusOrThrow(repository)
+
+      const { files } = afterRebase.workingDirectory
+
+      // omit the last change should cause Git to error because it requires
+      // all tracked changes to be staged as a prerequisite for rebasing
+      const onlyConflictedFiles = files.filter(f => f.path !== 'THIRD.md')
+
+      result = await continueRebase(repository, onlyConflictedFiles)
+    })
+
+    it('returns error code indicating that required files were missing', () => {
+      expect(result).toBe(ContinueRebaseResult.OutstandingFilesNotStaged)
+    })
+  })
 })

BenjaminJornet/open-desktop

app/test/unit/git/rebase/detect-conflict-test.ts
@@ -209,7 +209,7 @@ describe('git/rebase', () => {
   describe('continue with additional changes unrelated to conflicted files', () => {
     let beforeRebaseTip: Commit
     let filesInRebasedCommit: ReadonlyArray<CommittedFileChange>
-    let result: ContinueRebaseResult
+    let result: RebaseResult
     let status: IStatusResult
 
     beforeEach(async () => {
@@ -257,7 +257,7 @@ describe('git/rebase', () => {
     })
 
     it('returns success', () => {
-      expect(result).toBe(ContinueRebaseResult.CompletedWithoutError)
+      expect(result).toBe(RebaseResult.CompletedWithoutError)
     })
 
     it('keeps untracked working directory file out of rebase', () => {
@@ -276,6 +276,47 @@ describe('git/rebase', () => {
 
     it('branch is now a different ref', () => {
       expect(status.currentTip).not.toBe(beforeRebaseTip.sha)
+    })
+  })
+
+  describe('continue with tracked change omitted from list', () => {
+    let result: RebaseResult
+
+    beforeEach(async () => {
+      const repository = await createRepository(baseBranch, featureBranch)
+
+      await rebase(repository, baseBranch, featureBranch)
+
+      // resolve conflicts by writing files to disk
+      await FSE.writeFile(
+        Path.join(repository.path, 'THING.md'),
+        '# HELLO WORLD! \nTHINGS GO HERE\nFEATURE BRANCH UNDERWAY\n'
+      )
+
+      await FSE.writeFile(
+        Path.join(repository.path, 'OTHER.md'),
+        '# HELLO WORLD! \nTHINGS GO HERE\nALSO FEATURE BRANCH UNDERWAY\n'
+      )
+
+      // change unrelated tracked while rebasing changes
+      await FSE.writeFile(
+        Path.join(repository.path, 'THIRD.md'),
+        'this change should be included in the latest commit'
+      )
+
+      const afterRebase = await getStatusOrThrow(repository)
+
+      const { files } = afterRebase.workingDirectory
+
+      // omit the last change should cause Git to error because it requires
+      // all tracked changes to be staged as a prerequisite for rebasing
+      const onlyConflictedFiles = files.filter(f => f.path !== 'THIRD.md')
+
+      result = await continueRebase(repository, onlyConflictedFiles)
+    })
+
+    it('returns error code indicating that required files were missing', () => {
+      expect(result).toBe(RebaseResult.OutstandingFilesNotStaged)
     })
   })
 })